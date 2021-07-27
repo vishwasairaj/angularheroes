50315dc0fc5ee9d1f46d079f95465ce99ef46769
vishwasairaj <vanamalavishwasairaj@gmail.com>
2021-07-27 08:03:56 -0500

    mock-heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero} from './hero';
+export const HEROES: Hero[] = [
+    { id: 211, name: 'divya'},
+    { id: 216, name: 'harish'},
+    { id: 228, name: 'malli'},
+    { id: 230, name: 'vinnu'},
+    { id: 260, name: 'vishwa'},
+    { id: 311, name: 'sunny'},
+    { id: 343, name: 'bunny'}
+];
